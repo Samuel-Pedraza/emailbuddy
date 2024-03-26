d138d2cbcf2c53dc6ac3120f328125f8304cd440
karakhanyans <karakhanyansa@gmail.com>
2024-03-26 11:44:14 +0400

    Admin Dashboard Stats Improvements

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use LemonSqueezy\Laravel\Order;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +14,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(100)->create();
+        // LemonSqueezy package factory has a bug,
+        // For that reason forced to overwrite default values
+
+        for ($i = 0; $i < 300; $i++) {
+            try {
+                Order::factory()->create([
+                    'lemon_squeezy_id' => fake()->unique()->randomNumber(8),
+                    'order_number' => fake()->unique()->randomNumber(8),
+                    'customer_id' => fake()->unique()->randomNumber(8),
+                    'product_id' => fake()->unique()->randomNumber(8),
+                    'variant_id' => fake()->unique()->randomNumber(8),
+                    'subtotal' => $subtotal = rand(40000, 100000),
+                    'discount_total' => $discount = rand(1, 100),
+                    'tax' => $tax = rand(1, 50),
+                    'total' => $subtotal - $discount + $tax,
+                    'billable_id' => User::factory()->create(),
+                    'billable_type' => User::class,
+                    'identifier' => fake()->unique()->uuid()
+                ]);
+            } catch (\Exception $exception) {
+                continue;
+            }
+        }
+
+        // \App\Models\User::factory(100)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
