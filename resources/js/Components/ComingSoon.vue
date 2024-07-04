8ba1f9ad408ebfa084638f80d7eef8a74e5ab72c
karakhanyans <karakhanyansa@gmail.com>
2024-07-04 14:34:57 +0400

    Make using appName same for all pages

--- resources/js/Components/ComingSoon.vue
+++ resources/js/Components/ComingSoon.vue
@@ -1,10 +1,8 @@
 <script setup>
 
-import {Head, useForm, usePage} from "@inertiajs/vue3";
+import {Head, useForm} from "@inertiajs/vue3";
 import InputError from "@/Components/Profile/InputError.vue";
 import Banner from "@/Components/Profile/Banner.vue";
-
-const appName = usePage().props.appName;
 
 const form = useForm({
     email: '',
@@ -23,8 +21,8 @@ const submit = () => {
     <div class="hero min-h-screen bg-base-200">
         <div class="hero-content flex w-full flex-col sm:flex-row">
             <div class="basis-2/3 text-left mr-8">
-                <h1 class="text-5xl sm:text-7xl font-bold leading-12" v-html="$t('pages.coming-soon.title', { name: appName })"></h1>
-                <h2 class="py-6" v-html="$t('pages.coming-soon.description', { name: appName })"></h2>
+                <h1 class="text-5xl sm:text-7xl font-bold leading-12" v-html="$t('pages.coming-soon.title', { name: $page.props.appName })"></h1>
+                <h2 class="py-6" v-html="$t('pages.coming-soon.description', { name: $page.props.appName })"></h2>
             </div>
             <div class="basis-1/3">
                 <h3 class="text-4xl text-center mb-8">{{ $t('pages.coming-soon.waitlist.title') }}</h3>
@@ -32,7 +30,7 @@ const submit = () => {
                     <form class="card-body" @submit.prevent="submit">
                         <div class="form-control">
                             <label class="label mb-4">
-                                <span class="label-text text-xl" v-html="$t('pages.coming-soon.waitlist.description', { name: appName })"></span>
+                                <span class="label-text text-xl" v-html="$t('pages.coming-soon.waitlist.description', { name: $page.props.appName })"></span>
                             </label>
                             <input class="rounded mb-2 border-base-300" type="text" v-model="form.email" :placeholder="$t('pages.coming-soon.waitlist.email')">
                             <InputError class="mt-2" :message="form.errors.email"/>
